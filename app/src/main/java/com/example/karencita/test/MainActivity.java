a09925b614696e5545da17bc9f7545efaa460b35
Karencita <karenbauerpej@gmail.com>
2017-08-08 18:43:48 -0300

    open drawer when button clicks

--- app/src/main/java/com/example/karencita/test/MainActivity.java
+++ app/src/main/java/com/example/karencita/test/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.karencita.test;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
                                 Uri.parse("http://www.stackoverflow.com/"));
                 startActivity(viewIntent);
                 return true;
+            case android.R.id.home:
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -42,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
     private void initialize(){
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
